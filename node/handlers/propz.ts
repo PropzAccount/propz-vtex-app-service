9e6a679af72b4c4ce57556a738b5a06c217c304a
Adriano.A.Santos <aadriano990@gmail.com>
2023-06-13 10:43:34 -0300

    Ajustado metodos para salvar verifyPurchage e registerPurchage

--- node/handlers/propz.ts
+++ node/handlers/propz.ts
@@ -1,59 +1,29 @@
 /* eslint-disable no-console */
 /* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { json } from 'co-body'
 
 // eslint-disable-next-line prettier/prettier
 import type { Items } from '../types/Items'
+
 const getAppId = (): string => {
   return process.env.VTEX_APP_ID ?? ''
 }
-
-export async function teste(context: Context, next: () => Promise<any>) {
-  const {
-    clients: { Propz, Vtex, apps },
-    vtex: {
-      route: { params },
-    },
-  } = context
-
-  const { skuId } = params
-
-  const app: string = getAppId()
-  const settings = await apps.getAppSettings(app)
-
-  const res = await Vtex.getSkuAndContext('lojasantoantonio', `${skuId}`)
-  console.log(res)
-
-  const response = await Propz.getPromotion(
-    settings.domain,
-    settings.token,
-    43012319867,
-    settings.username,
-    settings.password
-  )
-
-  context.body = response
-  await next()
-}
-
-// get iFrame promotion
+  
 export async function getPromotion(ctx: Context, next: () => Promise<any>) {
   const {
     clients: { Propz, Vtex,  apps },
+    vtex: {
+      account
+    }
   } = ctx
 
   const app: string = getAppId()
-  const settings = await apps.getAppSettings(app)
+  const { domain, token, username, password, storeId } = await apps.getAppSettings(app)
 
   const { query } = ctx.request
 
-  const validation = await Propz.checkFields([
-    settings.domain,
-    settings.token,
-    settings.username,
-    settings.password,
-    settings.storeId,
-  ])
+  const validation = await Propz.checkFields([domain, token, username, password, storeId])
 
   const err = {
     success: false,
@@ -66,20 +36,14 @@ export async function getPromotion(ctx: Context, next: () => Promise<any>) {
   }
 
   try {
-    const response = await Propz.getPromotion(
-      settings.domain,
-      settings.token,
-      query.document,
-      settings.username,
-      settings.password
-    )
+    const response = await Propz.getPromotion(domain, token, query.document, username, password)
 
     const data = response.items.reduce(async ( acc: any , propzItem: Items ) => {
       const productRefPropz = propzItem.promotion.properties.PRODUCT_ID_INCLUSION
 
       if(propzItem.active && propzItem.promotion.active){
 
-        const [ vtexData ] = await Vtex.getSkuAndContext('lojasantoantonio',  productRefPropz)
+        const [ vtexData ] = await Vtex.getSkuAndContext(account,  productRefPropz)
         
         const productRefVtex = vtexData.items[0].referenceId[0].Value
         
@@ -123,10 +87,99 @@ export async function getPromotion(ctx: Context, next: () => Promise<any>) {
     }, [])
 
     ctx.status = 200
+    ctx.body = data
+  } catch (error) {
+    ctx.status = 400
+    ctx.body = error
+  }
+
+  ctx.set('cache-control', 'no-cache')
+  await next()
+}
+
+export async function getPromotionMassive (ctx: Context, next: () => Promise<any>) {
+  const {
+    clients: { Propz, Vtex,  apps },
+    vtex: {
+      account
+    }
+  } = ctx
+
+  const app: string = getAppId()
+  const { domain, token, username, password, storeId } = await apps.getAppSettings(app)
+
+  const { query } = ctx.request
+
+  const validation = await Propz.checkFields([domain, token, username, password, storeId])
+
+  const err = {
+    success: false,
+    message: 'fill in all fields within the admin',
+  }
+
+  if (!validation) {
+    ctx.status = 400
+    ctx.body = err
+  }
+
+  try {
+    const response = await Propz.getPromotionMassive(domain, token, query.document, username, password)
+
+    // eslint-disable-next-line prefer-const
+    const data = response.items.reduce(async ( acc: any , propzItem: Items) => {
+      const productRefPropz = propzItem.promotion.properties.PRODUCT_ID_INCLUSION
+
+      if(propzItem.active && propzItem.promotion.active){
+
+        const [ vtexData ] = await Vtex.getSkuAndContext(account,  productRefPropz)
+        
+        const productRefVtex = vtexData.items[0].referenceId[0].Value
+        
+        if(productRefVtex === productRefPropz){
+          console.log(productRefVtex === productRefPropz)
+         
+          acc.push({
+            productId: vtexData.productId,
+            description: vtexData.description,
+            productName: vtexData.productName,
+            productReference: vtexData.productReference,
+            linkText: vtexData.linkText,
+            brand: vtexData.brand,
+            brandId: vtexData.brandId,
+            link: vtexData.link,
+            categories: vtexData.categories,
+            categoryId: vtexData.categoryId,
+            priceRange: {
+              sellingPrice: {
+                highPrice: propzItem.promotion.finalPrice || 10.99,
+                lowPrice: propzItem.promotion.finalPrice || 10.99,
+                __typename: 'PriceRange',
+              },
+              listPrice: {
+                highPrice: propzItem.promotion.finalPrice || 20.99,
+                lowPrice: propzItem.promotion.finalPrice || 20.99,
+                __typename: 'PriceRange',
+              },
+              __typename: 'ProductPriceRange',
+            },
+            productClusters: vtexData.productClusters,
+            clusterHighlights: vtexData.clusterHighlights,
+            __typename: 'Product',
+            items: vtexData.items,
+            rule: null,
+            sku: vtexData.items[0],
+          })
+        }
+      }
+
+      return acc
+    }, [])
+    
+    ctx.status = 200
     ctx.body = await data
   } catch (error) {
     ctx.status = 400
-    ctx.body = err
+    ctx.body = error
   }
 
   ctx.set('cache-control', 'no-cache')
@@ -139,27 +192,37 @@ export async function postVerifyPurchase(
 ) {
   const {
     clients: { Propz, apps },
-    // vtex: {
-    //   route: { params },
-    // },
   } = ctx
-
+ 
   const app: string = getAppId()
-  const settings = await apps.getAppSettings(app)
+  const { domain, token, username, password } = await apps.getAppSettings(app)
 
-  const items: never[] = []
+  const validation = await Propz.checkFields([domain, token, username, password])
 
-  const response = await Propz.postVerifyPurchase(
-    settings.domain,
-    settings.token,
-    settings.username,
-    settings.password,
-    items
-  )
+  const err = {
+    success: false,
+    message: 'fill in all fields within the admin',
+  }
 
-  ctx.status = 200
-  ctx.body = response
+  if (!validation) {
+    ctx.status = 400
+    ctx.body = err
+  }
 
+  try {
+    const data = await json(ctx.req)
+  
+    const response = await Propz.postVerifyPurchase(domain, token, username, password, data)
+  
+    ctx.status = 200
+    ctx.body = response
+  } catch (error) {
+    ctx.status = 400
+    ctx.body = error
+  }
+
+
+  ctx.set('cache-control', 'no-cache')
   await next()
 }
 
@@ -169,148 +232,36 @@ export async function postRegisterPurchase(
 ) {
   const {
     clients: { Propz, apps },
-    // vtex: {
-    //   route: { params },
-    // },
   } = ctx
 
   const app: string = getAppId()
-  const settings = await apps.getAppSettings(app)
+  const { domain, token, username, password } = await apps.getAppSettings(app)
 
-  const items: never[] = []
+  const validation = await Propz.checkFields([domain,token,username,password ])
 
-  const response = await Propz.postRegisterPurchase(
-    settings.domain,
-    settings.token,
-    settings.username,
-    settings.password,
-    items
-  )
-
-  ctx.status = 200
-  ctx.body = response
-
-  await next()
-}
-
-export async function getFormUrl(ctx: Context, next: () => Promise<any>) {
-  const {
-    clients: { apps },
-  } = ctx
-
-  const app: string = getAppId()
-  const settings = await apps.getAppSettings(app)
-
-  ctx.status = 200
-  const response = {
-    formUrl: settings.formUrl,
-    storeId: settings.storeId,
+  const err = {
+    success: false,
+    message: 'fill in all fields within the admin',
   }
 
-  ctx.body = response
+  if (!validation) {
+    ctx.status = 400
+    ctx.body = err
+  }
+
+  try {
+    const data = await json(ctx.req)
+  
+    const response = await Propz.postRegisterPurchase(domain, token, username, password, data)
+  
+    ctx.status = 200
+    ctx.body = response
+  } catch (error) {
+    ctx.status = 400
+    ctx.body = error
+  }
+
   ctx.set('cache-control', 'no-cache')
   await next()
 }
 
-export async function getPromotionJson(ctx: Context, next: () => Promise<any>) {
-  const {
-    clients: { Propz, apps },
-  } = ctx
-
-  const { query } = ctx.request
-
-  const app: string = getAppId()
-  const settings = await apps.getAppSettings(app)
-
-  const validation = await Propz.checkFields([
-    settings.domain,
-    settings.token,
-    settings.username,
-    settings.password,
-    settings.storeId,
-  ])
-
-  const body = validation
-    ? await Propz.getPromotionJson(
-        settings.domain,
-        settings.token,
-        query.user,
-        settings.username,
-        settings.password
-      )
-    : { success: false, message: 'fill in all fields within the admin' }
-
-  ctx.status = 200
-  ctx.body = body
-  ctx.set('cache-control', 'no-cache')
-  await next()
-}
-
-export async function getProductList(ctx: Context, next: () => Promise<any>) {
-  const {
-    clients: { Propz, apps },
-  } = ctx
-
-  const app: string = getAppId()
-  const settings = await apps.getAppSettings(app)
-
-  const { query } = ctx.request
-
-  const validation = await Propz.checkFields([
-    settings.domain,
-    settings.token,
-    settings.username,
-    settings.password,
-    settings.storeId,
-  ])
-
-  const body = validation
-    ? await Propz.getProductList(
-        settings.domain,
-        settings.token,
-        query.document,
-        settings.username,
-        settings.password
-      )
-    : { success: false, message: 'fill in all fields within the admin' }
-
-  ctx.status = 200
-
-  ctx.body = body
-  ctx.set('cache-control', 'no-cache')
-  await next()
-}
-
-export async function postPromotion(ctx: Context, next: () => Promise<any>) {
-  const {
-    clients: { Propz, apps },
-  } = ctx
-
-  const app: string = getAppId()
-  const settings = await apps.getAppSettings(app)
-  const reqBody = await json(ctx.req)
-
-  const validation = await Propz.checkFields([
-    settings.domain,
-    settings.token,
-    settings.username,
-    settings.password,
-    settings.storeId,
-  ])
-
-  const body = validation
-    ? await Propz.postPromotion(
-        settings.domain,
-        settings.token,
-        settings.username,
-        settings.password,
-        reqBody
-      )
-    : { success: false, message: 'fill in all fields within the admin' }
-
-  ctx.status = 200
-
-  ctx.body = body
-  ctx.set('cache-control', 'no-cache')
-  await next()
-}
